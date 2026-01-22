c06e11907d59da4f6ac02972cf5644983d957894
Rebekah Gall <r.dickerson120@gmail.com>
2026-01-21 18:38:11 -0600

    omfg Im dumb, dont need an entry function here
    omfg Im dumb, dont need an entry function here

--- Day-01/program.ts
+++ Day-01/program.ts
@@ -1,7 +1,6 @@
 import {processDialTurns} from "./dial";
 import * as fs from 'fs';
 
-function program(){
     const filePath: string = 'turns.txt';
     const content: string = fs.readFileSync(filePath, 'utf-8');
     const dialTurns: string[] = content.split(/\r?\n/);
@@ -9,4 +8,4 @@ function program(){
     let elfWords = processDialTurns(dialTurns);
 
     console.log(`The elven password should be: ${elfWords}`);
-}
+
